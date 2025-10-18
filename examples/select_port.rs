fb0ed66fef41c6716d783b2e2018691c9f77820a
general electrix <general.electrix@gmail.com>
2025-10-18 01:29:53 -0700

    Increase test timeout.

--- examples/select_port.rs
+++ examples/select_port.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use rust_dmx::select_port;
 
 fn main() {
-    let mut port = select_port(Some(Duration::from_secs(1))).expect("failed to open port");
+    let mut port = select_port(Some(Duration::from_secs(10))).expect("failed to open port");
     println!("Opened port: \"{}\"", port);
     port.write(vec![0, 1, 2, 3, 4, 5].as_slice()).unwrap();
 }
