757c94940e97938fd1c619f758da56b6380f331e
general electrix <general.electrix@gmail.com>
2025-11-04 22:48:58 -0800

    Roll our own write method that avoids all the allocation.

--- src/artnet.rs
+++ src/artnet.rs
@@ -1,6 +1,6 @@
 //! Implementation of the artnet protocol as a DmxPort.
-use anyhow::{anyhow, Context};
-use artnet_protocol::{ArtCommand, Output, Poll};
+use anyhow::{anyhow, Context, Result};
+use artnet_protocol::{ArtCommand, Poll, PollReply};
 use log::{error, warn};
 use serde::{Deserialize, Serialize};
 
@@ -21,6 +21,8 @@ pub struct ArtnetDmxPort {
     socket: UdpSocket,
     #[serde(flatten)]
     params: ArtnetDmxPortParams,
+    #[serde(skip_serializing)]
+    send_buf: Vec<u8>,
 }
 
 impl TryFrom<ArtnetDmxPortParams> for ArtnetDmxPort {
@@ -29,6 +31,7 @@ impl TryFrom<ArtnetDmxPortParams> for ArtnetDmxPort {
         Ok(Self {
             socket: get_socket()?,
             params,
+            send_buf: vec![],
         })
     }
 }
@@ -36,6 +39,8 @@ impl TryFrom<ArtnetDmxPortParams> for ArtnetDmxPort {
 #[derive(Serialize, Deserialize)]
 struct ArtnetDmxPortParams {
     addr: Ipv4Addr,
+    /// The artnet internal port address.
+    port_address: u16,
     short_name: String,
     long_name: String,
 }
@@ -68,14 +73,28 @@ fn get_socket() -> anyhow::Result<UdpSocket> {
 }
 
 impl ArtnetDmxPort {
-    fn write(&mut self, frame: &[u8]) -> anyhow::Result<()> {
-        let command = ArtCommand::Output(Output {
-            data: frame.to_vec().into(),
-            ..Default::default()
-        });
-        let buffer = command.write_to_buffer()?;
+    fn from_poll(reply: &PollReply) -> Result<Self> {
+        Ok(Self {
+            socket: get_socket()?,
+            params: ArtnetDmxPortParams {
+                addr: reply.address,
+                port_address: u16::from_be_bytes(reply.port_address),
+                short_name: null_terminated_string_lossy(&reply.short_name).to_string(),
+                long_name: null_terminated_string_lossy(&reply.long_name).to_string(),
+            },
+            send_buf: vec![],
+        })
+    }
+
+    fn write(&mut self, frame: &[u8]) -> Result<()> {
+        // TODO: the first section of the packet is always the same
+        // we could pre-populate that. Probably not important, its a handful of
+        // bytes at most.
+        self.send_buf.clear();
+        send::write(&mut self.send_buf, self.params.port_address, frame)
+            .context("constructing artnet buffer")?;
         let dest = SocketAddrV4::new(self.params.addr, PORT);
-        self.socket.send_to(&buffer, dest)?;
+        self.socket.send_to(&self.send_buf, dest)?;
         Ok(())
     }
 }
@@ -83,7 +102,7 @@ impl ArtnetDmxPort {
 #[typetag::serde]
 impl DmxPort for ArtnetDmxPort {
     /// Poll for artnet devices
-    fn available_ports(wait: Duration) -> anyhow::Result<PortListing> {
+    fn available_ports(wait: Duration) -> Result<PortListing> {
         let socket = get_socket()?;
 
         let broadcast_addr = ("255.255.255.255", PORT)
@@ -112,14 +131,7 @@ impl DmxPort for ArtnetDmxPort {
             let command = ArtCommand::from_buffer(&buffer[..length])?;
 
             if let ArtCommand::PollReply(reply) = command {
-                ports.push(Box::new(Self {
-                    socket: get_socket()?,
-                    params: ArtnetDmxPortParams {
-                        addr: reply.address,
-                        short_name: null_terminated_string_lossy(&reply.short_name).to_string(),
-                        long_name: null_terminated_string_lossy(&reply.long_name).to_string(),
-                    },
-                }) as Box<dyn DmxPort>);
+                ports.push(Box::new(Self::from_poll(&reply)?) as Box<dyn DmxPort>);
             }
             Ok(())
         };
@@ -157,4 +169,91 @@ fn null_terminated_string_lossy(bytes: &[u8]) -> String {
         .position(|c| *c == b'\0')
         .unwrap_or(bytes.len());
     String::from_utf8_lossy(&bytes[0..null_pos]).to_string()
+}
+
+mod send {
+    //! The artnet_protocol library is way too eager to allocate memory on every
+    //! write to the port. This is a common issue with libraries that try to
+    //! represent an API as an enum (see also: the OSC library).
+
+    //! This little module implements just the code we need to write an artnet
+    //! DMX packet, with no allocations.
+    use anyhow::{ensure, Result};
+
+    use std::io::Write;
+
+    const ARTNET_HEADER: &[u8; 8] = b"Art-Net\0";
+    const ARTNET_PROTOCOL_VERSION: [u8; 2] = [0, 14];
+
+    /// Write the provided DMX buffer into the provided writer.
+    ///
+    /// The packet is addressed to the specified port address.
+    pub fn write(mut w: impl Write, arnet_port_address: u16, buf: &[u8]) -> Result<()> {
+        ensure!(!buf.is_empty(), "cannot send zero-length artnet frame");
+        ensure!(
+            buf.len() <= 512,
+            "artnet frame payload too long: {}",
+            buf.len()
+        );
+
+        let opcode: u16 = 0x5000;
+
+        w.write_all(ARTNET_HEADER)?;
+        // DMX output opcode.
+        w.write_all(&opcode.to_le_bytes())?;
+        w.write_all(&ARTNET_PROTOCOL_VERSION)?;
+        // Packet sequence number - we only care about intranet so always write 0.
+        write_u8(&mut w, 0)?;
+        // Physical input port number - not used, write 0.
+        write_u8(&mut w, 0)?;
+        // Destination port number.
+        w.write_all(&arnet_port_address.to_le_bytes())?;
+        let add_pad_byte = buf.len() % 2 != 0;
+        // Data payload length, rounded up to be a multiple of 2.
+        let padded_len = buf.len() as u16 + add_pad_byte as u16;
+        w.write_all(&padded_len.to_be_bytes())?;
+        w.write_all(buf)?;
+        if add_pad_byte {
+            write_u8(&mut w, 0)?;
+        }
+        Ok(())
+    }
+
+    fn write_u8(mut w: impl Write, v: u8) -> std::io::Result<()> {
+        let buf: [u8; 1] = [v];
+        w.write_all(&buf)
+    }
+
+    #[cfg(test)]
+    mod test {
+        use artnet_protocol::{ArtCommand, Output};
+
+        use super::write;
+        /// Ensure that our hacked-together write method produces identical results
+        /// as the artnet protocol library.
+        #[test]
+        fn test_match() {
+            for len in 1..512 {
+                let buf = vec![0u8; len];
+                assert_match(&buf);
+            }
+        }
+
+        fn write_vec(buf: &[u8]) -> Vec<u8> {
+            let mut w = vec![];
+            write(&mut w, 1, buf).unwrap();
+            w
+        }
+
+        fn assert_match(buf: &[u8]) {
+            let custom = write_vec(buf);
+            let library = ArtCommand::Output(Output {
+                data: buf.to_vec().into(),
+                ..Default::default()
+            })
+            .write_to_buffer()
+            .unwrap();
+            assert_eq!(library, custom);
+        }
+    }
 }
